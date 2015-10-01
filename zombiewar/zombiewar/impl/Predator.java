16df81a8459152a37664f0abbfc220f463f50a82
AlexLaurent11 <LaurentAM03@uww.edu>
2015-10-01 15:06:22 -0500

    Predator has stats now

--- zombiewar/zombiewar/impl/Predator.java
+++ zombiewar/zombiewar/impl/Predator.java
@@ -5,15 +5,15 @@ import zombiewar.intf.IZombie;
 
 public class Predator extends Character implements IZombie{
 
-	Predator(int health) {
-		super(health);
+	public Predator(int health) {
+		super(120);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void attack(ISurvivor survivor) {
 		// TODO Auto-generated method stub
-		
+		survivor.decreaseHealth(10);
 	}
 
 }
