d512773707e420a7e5c41b18cb6109267ddfebac
AlexLaurent11 <LaurentAM03@uww.edu>
2015-10-01 15:09:31 -0500

    Child has stuff, but it sucks

--- zombiewar/zombiewar/impl/Child.java
+++ zombiewar/zombiewar/impl/Child.java
@@ -7,13 +7,14 @@ public class Child extends Character implements ISurvivor
 {
 
 	Child(int health) {
-		super(health);
+		super(25);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void attack(IZombie zombie) {
 		// TODO Auto-generated method stub
+		zombie.decreaseHealth(1);
 		
 	}
 
