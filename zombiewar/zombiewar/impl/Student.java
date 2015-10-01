20bc09eed534afd6923217332401c9c4b588e23e
AlexLaurent11 <LaurentAM03@uww.edu>
2015-10-01 15:11:57 -0500

    student has stats now

--- zombiewar/zombiewar/impl/Student.java
+++ zombiewar/zombiewar/impl/Student.java
@@ -6,14 +6,15 @@ import zombiewar.intf.IZombie;
 public class Student extends Character implements ISurvivor
 {
 
-	Student(int health) {
-		super(health);
+	public Student(int health) {
+		super(17);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void attack(IZombie zombie) {
 		// TODO Auto-generated method stub
+		zombie.decreaseHealth(5);
 		
 	}
 
