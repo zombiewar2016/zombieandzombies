6f382715215dfcf39f6010157aa87c921afec0b0
woodej01 <woodej01@uww.edu>
2015-10-01 23:53:43 -0500

    EricaSecondVersion
    Null-parameter polymorphic characters/zombies with empty inherited
    attack methods

--- zombiewar/impl/Child.java
+++ zombiewar/impl/Child.java
@@ -3,18 +3,17 @@ package zombiewar.impl;
 import zombiewar.intf.ISurvivor;
 import zombiewar.intf.IZombie;
 
-public class Student extends Character implements ISurvivor
+public class Child extends Character implements ISurvivor
 {
 
-	public Student(int health) {
-		super(17);
+	Child() {
+		super(25);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void attack(IZombie zombie) {
 		// TODO Auto-generated method stub
-		zombie.decreaseHealth(5);
 		
 	}
 
--- zombiewar/impl/Predator.java
+++ zombiewar/impl/Predator.java
@@ -5,15 +5,16 @@ import zombiewar.intf.IZombie;
 
 public class Predator extends Character implements IZombie{
 
-	public Predator(int health) {
+	public Predator()
+	{
 		super(120);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void attack(ISurvivor survivor) {
+	public void attack(ISurvivor survivor)
+	{
 		// TODO Auto-generated method stub
-		survivor.decreaseHealth(10);
+		
 	}
 
 }
